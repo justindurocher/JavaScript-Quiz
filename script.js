10a9e29fb1cf4254d277101e05fb10aadcc544b2
Justin Durocher <justduro@gmail.com>
2019-12-19 12:31:47 -0500

    Jquery conversion

--- script.js
+++ script.js
@@ -1,66 +1,46 @@
-var timer = document.querySelector("#seconds");
-var restartButton = document.querySelector(".restart");
-var submitButton = document.querySelector(".submit");
-var initials = document.querySelector("#initials");
-var input = document.querySelector(".input");
-var promptArea = document.querySelector(".prompt");
-var welcome = document.querySelector(".welcome");
-var content = document.querySelector(".content");
-var quizCard1 = document.querySelector(".quiz-1");
-var quizCard2 = document.querySelector(".quiz-2");
-var quizCard3 = document.querySelector(".quiz-3");
-var quizCard4 = document.querySelector(".quiz-4");
-var quizCard5 = document.querySelector(".quiz-5");
-var buttonA = document.querySelector(".A");
-var buttonB = document.querySelector(".B");
-var buttonC = document.querySelector(".C");
-var buttonD = document.querySelector(".D");
-var buttonE = document.querySelector(".E");
-var buttonF = document.querySelector(".F");
-var buttonG = document.querySelector(".G");
-var buttonH = document.querySelector(".H");
-var buttonI = document.querySelector(".I");
-var buttonJ = document.querySelector(".J");
-var buttonK = document.querySelector(".K");
-var buttonL = document.querySelector(".L");
-var buttonM = document.querySelector(".M");
-var buttonN = document.querySelector(".N");
-var buttonO = document.querySelector(".O");
-var buttonP = document.querySelector(".P");
-var buttonQ = document.querySelector(".Q");
-var buttonR = document.querySelector(".R");
-var buttonS = document.querySelector(".S");
-var buttonT = document.querySelector(".T");
-var question = document.querySelector(".question");
-var startingScore = document.querySelector("#score");
-var quizCardFinal = document.querySelector(".quiz-final");
-var finalSeconds = document.querySelector(".final-seconds");
-var finalScore = document.querySelector(".final-score");
+var timer = $("#seconds");
+var restartButton = $(".restart");
+var submitButton = $(".submit");
+var initials = $("#initials");
+var input = $(".input");
+var promptArea = $(".prompt");
+var welcome = $(".welcome");
+var content = $(".content");
+var quizCard1 = $(".quiz-1");
+var quizCard2 = $(".quiz-2");
+var quizCard3 = $(".quiz-3");
+var quizCard4 = $(".quiz-4");
+var quizCard5 = $(".quiz-5");
+var question = $(".question");
+var startingScore = $("#score");
+var quizCardFinal = $(".quiz-final");
+var finalSeconds = $(".final-seconds");
+var finalScore = $(".final-score");
     var secondsLeft = 30;
     var score = 0;
- restartButton.addEventListener("click", reload);
+ restartButton.on("click", reload);
  function reload(){
      document.location.reload();
  }
  function setTime() {
      var timerInterval = setInterval(function(){
          secondsLeft--;
-         timer.textContent = secondsLeft + "s";
+         timer.text(secondsLeft + "s");
 
-         if(secondsLeft===0 || score===25){
+         if(score===25 || secondsLeft===0){
              clearInterval(timerInterval);
              alert("Time is up!");
-             timer.textContent = "None!";
-             quizCard1.classList.add('hide');
-             quizCard2.classList.add('hide');
-             quizCard3.classList.add('hide');
-             quizCard4.classList.add('hide');
-             quizCard5.classList.add('hide');
-             quizCardFinal.classList.remove('hide');
+             timer.text("None!");
+             quizCard1.addClass('hide');
+             quizCard2.addClass('hide');
+             quizCard3.addClass('hide');
+             quizCard4.addClass('hide');
+             quizCard5.addClass('hide');
+             quizCardFinal.removeClass('hide');
          }
      }, 1200);
  } 
-submitButton.addEventListener("click",function(){
+submitButton.on("click",function(){
     setTime();
     quiz();
 });
--- script2.js
+++ script2.js
@@ -1,86 +1,136 @@
-var timer = document.querySelector("#seconds");
-var restartButton = document.querySelector(".restart");
-var submitButton = document.querySelector(".submit");
-var initials = document.querySelector("#initials");
-var input = document.querySelector(".input");
-var promptArea = document.querySelector(".prompt");
-var welcome = document.querySelector(".welcome");
-var content = document.querySelector(".content");
-var quizCard1 = document.querySelector(".quiz-1");
-var quizCard2 = document.querySelector(".quiz-2");
-var quizCard3 = document.querySelector(".quiz-3");
-var quizCard4 = document.querySelector(".quiz-4");
-var quizCard5 = document.querySelector(".quiz-5");
-var buttonA = document.querySelector(".A");
-var buttonB = document.querySelector(".B");
-var buttonC = document.querySelector(".C");
-var buttonD = document.querySelector(".D");
-var buttonE = document.querySelector(".E");
-var buttonF = document.querySelector(".F");
-var buttonG = document.querySelector(".G");
-var buttonH = document.querySelector(".H");
-var buttonI = document.querySelector(".I");
-var buttonJ = document.querySelector(".J");
-var buttonK = document.querySelector(".K");
-var buttonL = document.querySelector(".L");
-var buttonM = document.querySelector(".M");
-var buttonN = document.querySelector(".N");
-var buttonO = document.querySelector(".O");
-var buttonP = document.querySelector(".P");
-var buttonQ = document.querySelector(".Q");
-var buttonR = document.querySelector(".R");
-var buttonS = document.querySelector(".S");
-var buttonT = document.querySelector(".T");
-var question = document.querySelector(".question");
-var startingScore = document.querySelector("#score");
-var quizCardFinal = document.querySelector(".quiz-final");
-var finalSeconds = document.querySelector(".final-seconds");
-var finalScore = document.querySelector(".final-score");
-   var firstPlace = document.querySelector(".first-place");
-   var secondPlace = document.querySelector(".second-place");    
-   var thirdPlace = document.querySelector(".third-place");
-   var fourthPlace = document.querySelector(".fourth-place");
-   var fifthPlace = document.querySelector(".fifth-place");
+function quiz(){
+var timer = $("#seconds");
+var restartButton = $(".restart");
+var submitButton = $(".submit");
+var initials = $("#initials");
+var input = $(".input");
+var promptArea = $(".prompt");
+var welcome = $(".welcome");
+var content = $(".content");
+var quizCard1 = $(".quiz-1");
+var quizCard2 = $(".quiz-2");
+var quizCard3 = $(".quiz-3");
+var quizCard4 = $(".quiz-4");
+var quizCard5 = $(".quiz-5");
+var question = $(".question");
+var startingScore = $("#score");
+var quizCardFinal = $(".quiz-final");
+var finalSeconds = $(".final-seconds");
+var finalScore = $(".final-score");
     var secondsLeft = 30;
     var score = 0;
-function quiz(){
    localStorage.setItem("name",initials.value);
-    submitButton.classList.add('hide');
-    promptArea.classList.add('hide');
-    welcome.classList.add('hide');
-    content.classList.add('hide');
-    quizCard1.classList.remove('hide');
-    buttonB.addEventListener("click", function(){
-       quizCard1.classList.add('hide');
-       quizCard2.classList.remove('hide');
-       score+=5;
-       startingScore.textContent = score;
+    submitButton.addClass('hide');
+    promptArea.addClass('hide');
+    welcome.addClass('hide');
+    content.addClass('hide');
+    quizCard1.removeClass('hide');
+    $(".A").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".A").addClass('incorrect');
     });
-    buttonG.addEventListener("click",function(){
-       quizCard2.classList.add('hide');
-       quizCard3.classList.remove('hide');
+    $(".B").on("click", function(){
+       quizCard1.addClass('hide');
+       quizCard2.removeClass('hide');
        score+=5;
-       startingScore.textContent = score;
+       startingScore.text(score);
     });
-    buttonL.addEventListener("click",function(){
-       quizCard3.classList.add('hide');
-       quizCard4.classList.remove('hide');
-       score+=5;
-       startingScore.textContent = score;
+    $(".C").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".C").addClass('incorrect');
     });
-    buttonO.addEventListener("click",function(){
-       quizCard4.classList.add('hide');
-       quizCard5.classList.remove('hide');
-       score+=5;
-       startingScore.textContent = score;
+    $(".D").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".D").addClass('incorrect');
     });
-    buttonQ.addEventListener("click",function(){
-       quizCard5.classList.add('hide');
-       quizCardFinal.classList.remove('hide');
+    $(".E").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".E").addClass('incorrect');
+    });
+    $(".F").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".F").addClass('incorrect');
+    });
+    $(".G").on("click",function(){
+       quizCard2.addClass('hide');
+       quizCard3.removeClass('hide');
        score+=5;
-       startingScore.textContent = score;
-       finalSeconds.textContent = secondsLeft;
-       finalScore.textContent = score + secondsLeft;
-       localStorage.setItem("score",finalScore.textContent);
+       startingScore.text(score);
+    });
+    $(".H").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".H").addClass('incorrect');
+    });
+    $(".I").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".I").addClass('incorrect');
+    });
+    $(".J").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".J").addClass('incorrect');
+    });
+    $(".K").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".K").addClass('incorrect');
+    });
+    $(".L").on("click",function(){
+       quizCard3.addClass('hide');
+       quizCard4.removeClass('hide');
+       score+=5;
+       startingScore.text(score);
+    });
+    $(".M").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".M").addClass('incorrect');
+    });
+    $(".N").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".N").addClass('incorrect');
+    });
+    $(".O").on("click",function(){
+       quizCard4.addClass('hide');
+       quizCard5.removeClass('hide');
+       score+=5;
+       startingScore.text(score);
+    });
+    $(".P").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".P").addClass('incorrect');
+    });
+    $(".Q").on("click",function(){
+       quizCard5.addClass('hide');
+       quizCardFinal.removeClass('hide');
+       score+=5;
+       startingScore.text(score);
+       finalSeconds.text(secondsLeft);
+       finalScore.text(score+secondsLeft);
+       localStorage.setItem("score",finalScore.text());
+    });
+    $(".R").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".R").addClass('incorrect');
+    });
+    $(".S").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".S").addClass('incorrect');
+    });
+    $(".T").on("click",function(){
+      secondsLeft-=2;
+      timer.text(secondsLeft);
+      $(".T").addClass('incorrect');
     });
 }
